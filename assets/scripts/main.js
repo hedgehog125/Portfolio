a742c0ee46034a8e039b6c1d501b9b348ffa8059
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-09-20 08:18:04 +0100

    Started changing the spacing. Think I've made it worse apart from removing the underline

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -36,27 +36,20 @@ const game = Bagel.init({
                     },
                     {
                         type: "text",
-                        color: "#EFEFEF",
+                        color: "white",
                         text: "About Me",
                         size: 40,
                         x: 525,
-                        y: 25,
-                        submenu: "main"
-                    },
-                    {
-                        type: "image",
-                        color: "#EFEFEF",
-                        width: 175,
-                        x: 525,
-                        y: 42,
-                        height: 3,
+                        y: 75,
                         submenu: "main"
                     },
                     {
                         type: "text",
                         color: "#EFEFEF",
                         x: 525,
+                        y: 270,
                         text: "Hi, I'm, Nico Clack and I'm mostly programming in JavaScript at the moment. My main 3 hobbies are programming, watching videos and playing videogames but I'm working on expanding my interests by listening to audio books, cooking and swimming more.\n\nI've been programming since I was about 8 and a half so I've produced a fair bit over the years (I'm 17). My older stuff is under hedgehog125 and earlier nicoclack on Scratch.\n\nAnyway, scroll down for my semi recent projects...",
+                        size: 18,
                         wordWrapWidth: 450,
                         submenu: "main"
                     },
