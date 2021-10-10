08693e42ec263c574cbc95478b47097be4abf792
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2021-10-10 16:07:34 +0100

    Added hover text, tidied up some code to reduce repetition and added the Froniter page.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,27 @@
+/*
+TODO
+Improve play icon
+Compress images
+
+
+*/
+
 const game = (_ => {
-    const githubHandler = (menuSprite, animationVars) => {
+    const externalHandler = (menuSprite, animationVars) => {
         if (! game.vars.loadingExternalPage) {
             if (game.vars.externalPageDelay == 0) {
                 // It'll take a second or two to load anyway so start loading while the animation is active
                 let projectID = menuSprite.submenu.split(".");
+                let type = projectID[projectID.length - 1];
                 projectID.splice(0, 1);
                 projectID.splice(projectID.length - 1, 1);
                 projectID = projectID.join(".");
-                location.href = menuSprite.vars.githubURLs[projectID];
+                if (type == "GitHub") {
+                    location.href = menuSprite.vars.githubURLs[projectID];
+                }
+                else {
+                    location.href = menuSprite.vars.pagesURLs[projectID];
+                }
                 game.vars.loadingExternalPage = true;
             }
             else {
@@ -15,6 +29,158 @@ const game = (_ => {
             }
         }
     };
+    const transitionSubmenu = (name, type) => {
+        let submenuID = "Project." + name + "." + type;
+        let ob = {};
+        ob[submenuID] = {
+            elements: [
+                {
+                    type: "image",
+                    color: "black",
+                    width: 800,
+                    height: 450
+                }
+            ],
+            animationMain: externalHandler
+        };
+        return ob;
+    };
+    const projectBar = (submenu, pages) => {
+        let elements = [{
+            type: "image",
+            color: "#202020",
+            width: 800,
+            height: 95,
+            fixedToCamera: true,
+            bottom: 450
+        },
+        {
+            type: "button",
+            icon: "GitHub",
+            color: "black",
+            onHover: "View on GitHub",
+            onClick: {
+                submenu: "Project." + submenu + ".GitHub",
+                animation: {
+                    type: "triangleScroll"
+                }
+            },
+            size: 50,
+            x: 400,
+            y: 400,
+            fixedToCamera: true
+        }];
+        if (pages) {
+            elements[1].x = 360;
+            elements.push({
+                type: "button",
+                icon: "Play",
+                color: "lime",
+                onHover: pages == 2? "Run it" : "Play it",
+                onClick: {
+                    submenu: "Project." + submenu + ".Pages",
+                    animation: {
+                        type: "triangleScroll"
+                    }
+                },
+                size: 50,
+                x: 440,
+                y: 400,
+                fixedToCamera: true
+            });
+        }
+        return elements;
+    };
+    const title = text => ({
+        type: "text",
+        color: "white",
+        text: text,
+        size: 40,
+        y: 38
+    });
+    const basicInfo = (releaseDate, status, latestVersion) => ({
+        type: "text",
+        color: "#EFEFEF",
+        left: 25,
+        top: 80,
+        text:
+`Release Date: ${releaseDate}
+Status: ${status}
+Latest Version: ${latestVersion}`,
+        size: 15,
+        wordWrapWidth: 250
+    });
+    const sideText = (text, yOffset = 0) => ({
+        type: "text",
+        color: "#EFEFEF",
+        left: 301,
+        top: 75 + yOffset,
+        text: text,
+        size: 20,
+        wordWrapWidth: 474
+    });
+    const paragraph = (text, y) => ({
+        type: "text",
+        color: "#EFEFEF",
+        left: 25,
+        top: y,
+        text: text,
+        size: 20,
+        wordWrapWidth: 750
+    });
+
+    const background = _ => ({
+        type: "image",
+        color: "#202020",
+        width: 800,
+        height: 450,
+        fixedToCamera: true
+    });
+    const hoverText = lower => ({
+        hoverText: {
+            color: "white",
+            ...(lower? {bottom: 450} : {})
+        }
+    });
+    const backButton = (x, y, color) => {
+        return {
+            type: "button",
+            icon: "Back",
+            color: "white",
+            onClick: {
+                submenu: "main",
+                animation: {
+                    type: "circle",
+                    color: color,
+                    direction: "close",
+                    x: x,
+                    y: y
+                }
+            },
+            size: 50,
+            x: 50,
+            y: 50,
+            fixedToCamera: true
+        };
+    };
+    const projectButton = (name, color, x, y, hover) => {
+        return {
+            type: "button",
+            color: color,
+            icon: "ProjectIcon." + name,
+            x: x,
+            y: y,
+            size: 100,
+            onHover: hover,
+            onClick: {
+                submenu: "Project." + name,
+                animation: {
+                    type: "circle"
+                }
+            }
+        };
+    };
+
     return Bagel.init({
         id: "menu",
         state: "menu",
@@ -44,11 +210,24 @@ const game = (_ => {
                         id: "Back",
                         src: "assets/imgs/backArrow.png"
                     },
+                    {
+                        id: "Play",
+                        src: "assets/imgs/play.png"
+                    },
 
                     {
                         id: "ProjectIcon.BagelJS",
                         src: "assets/imgs/projectIcons/bagelJS.png",
                         webP: "assets/imgs/projectIcons/bagelJS.webp",
+                        upscale: {
+                            width: 150,
+                            height: 150,
+                            antialias: false
+                        }
+                    },
+                    {
+                        id: "ProjectIcon.Frontier",
+                        src: "assets/imgs/projectIcons/frontier.png",
                         upscale: {
                             width: 150,
                             height: 150,
@@ -63,7 +242,11 @@ const game = (_ => {
                     type: "GUI",
                     vars: {
                         githubURLs: {
-                            BagelJS: "https://github.com/hedgehog125/Bagel.js"
+                            BagelJS: "https://github.com/hedgehog125/Bagel.js",
+                            Frontier: "https://github.com/hedgehog125/Frontier-Fan-Made-Game"
+                        },
+                        pagesURLs: {
+                            Frontier: "https://hedgehog125.github.io/Frontier-Fan-Made-Game/"
                         }
                     },
                     submenu: "main",
@@ -114,148 +297,75 @@ Anyway, scroll down for my semi recent projects...`,
                                     y: 450 + 42,
                                     height: 3
                                 },
-                                {
-                                    type: "button",
-                                    color: "yellow",
-                                    icon: "ProjectIcon.BagelJS",
-                                    y: 450 + 225,
-                                    size: 100,
-                                    onclick: {
-                                        submenu: "Project.BagelJS",
-                                        animation: {
-                                            type: "circle"
-                                        }
-                                    }
-                                }
+
+                                projectButton("BagelJS", "yellow", 300, 450 + 225, "Bagel.js"),
+                                projectButton("Frontier", "black", 500, 450 + 225, "Frontier")
                             ],
                             scroll: {
                                 y: {
                                     min: 0,
                                     max: 450
                                 }
-                            }
+                            },
+                            ...hoverText()
                         },
                         "Project.BagelJS": {
                             elements: [
-                                {
-                                    type: "image",
-                                    color: "#202020",
-                                    width: 800,
-                                    height: 450,
-                                    fixedToCamera: true
-                                },
-                                {
-                                    type: "text",
-                                    color: "white",
-                                    text: "Bagel.js",
-                                    size: 40,
-                                    y: 38
-                                },
-                                {
-                                    type: "text",
-                                    color: "#EFEFEF",
-                                    left: 25,
-                                    top: 80,
-                                    text:
-`Release Date: 22/6/20 (1.0b) / 7/2/19 (game.js uploaded onto GitHub)
-Status: Paused
-    Latest Version: 1.5.1b/1.6a (WIP)`,
-                                    size: 15,
-                                    wordWrapWidth: 250
-                                },
-                                {
-                                    type: "text",
-                                    color: "#EFEFEF",
-                                    left: 301,
-                                    top: 75,
-                                    text:
-`Bagel.js is a 2D JavaScript games framework where objects and arrays are heavilly used to structure data and code. The idea being that it reduces repetition for commands such as creating a sprite.`,
-                                    size: 20,
-                                    wordWrapWidth: 474
-                                },
-                                {
-                                    type: "text",
-                                    color: "#EFEFEF",
-                                    left: 25,
-                                    top: 202,
-                                    text:
+                                background(),
+                                title("Bagel.js"),
+                                basicInfo(
+                                    "22/6/20 (1.0b) / 7/2/19 (game.js uploaded onto GitHub)",
+                                    "Paused",
+                                    "1.5.1b/1.6a (WIP)"
+                                ),
+                                sideText(`Bagel.js is a 2D JavaScript games framework where objects and arrays are heavily used to structure data and code. The idea being that it reduces repetition for commands such as creating a sprite.`),
+                                paragraph(
 `It's technically based on my previous game framework: Begining.js but most of the underlying code was significantly changed so syntax is the main similarity. (also similar to the previous previous: JAMESCRIPT) They also share the same GitHub repository so Begining.js isn't listed here.
 
-Looking back, I'm conflicted about Bagel.js. It's been an interesting learning and it's definitely my best framework so far. However, it was designed for the organisation needed for large projects, in a language primarily designed for simpler programs. Ironically, it looks like I'm going to be using it for simple projects as I have my UI plugin. Especially since I'm now much more comfortable with Unity as I'm using it on my Games Development course.`,
-                                    size: 20,
-                                    wordWrapWidth: 750
-                                },
-                                {
-                                    type: "text",
-                                    color: "#EFEFEF",
-                                    left: 25,
-                                    top: 507,
-                                    text:
+Looking back, I'm conflicted about Bagel.js. It's been an interesting learning opportunity and it's definitely my best framework so far. However, it was designed for the organisation needed for large projects, in a language primarily designed for simpler programs. Ironically, it looks like I'm going to be using it for simple projects as I have my UI plugin. Especially since I'm now much more comfortable with Unity as I'm using it on my Games Development course.`,
+                                202),
+                                paragraph(
 `I guess it's a reflection of my obsession to do things my way, often to a fault. Still, at least it's a lesson and I imagine the more programming teamwork I do, the less I'll be like this. Either that or I'll convince them all to learn and use Bagel.js...`,
-                                    size: 20,
-                                    wordWrapWidth: 750
-                                },
+                                507),
 
-                                {
-                                    type: "image",
-                                    color: "#202020",
-                                    width: 800,
-                                    height: 75,
-                                    fixedToCamera: true,
-                                    bottom: 450
-                                },
-                                {
-                                    type: "button",
-                                    icon: "GitHub",
-                                    color: "black",
-                                    onclick: {
-                                        submenu: "Project.BagelJS.GitHub",
-                                        animation: {
-                                            type: "triangleScroll"
-                                        }
-                                    },
-                                    size: 65,
-                                    y: 412.5,
-                                    fixedToCamera: true
-                                },
-                                {
-                                    type: "button",
-                                    icon: "Back",
-                                    color: "white",
-                                    onclick: {
-                                        submenu: "main",
-                                        animation: {
-                                            type: "circle",
-                                            color: "yellow",
-                                            direction: "close",
-                                            x: 400,
-                                            y: 450 + 225
-                                        }
-                                    },
-                                    size: 50,
-                                    x: 25 + 25,
-                                    y: 25 + 25,
-                                    fixedToCamera: true
-                                }
+                                ...projectBar("BagelJS"),
+                                backButton(300, 450 + 225, "yellow")
                             ],
                             scroll: {
                                 y: {
                                     min: 0,
                                     max: 233
                                 }
-                            }
+                            },
+                            ...hoverText(true)
                         },
-                        "Project.BagelJS.GitHub": {
+                        ...transitionSubmenu("BagelJS", "GitHub"),
+
+                        "Project.Frontier": {
                             elements: [
-                                {
-                                    type: "image",
-                                    color: "black",
-                                    width: 800,
-                                    height: 450
-                                }
+                                background(),
+                                title("Frontier"),
+                                basicInfo(
+                                    "31/11/17 (v1.0)",
+                                    "Discontinued/semi finished",
+                                    "v1.1"
+                                ),
+                                sideText(`Frontier is a HTML5 space shooter game inspired by the Scratch game: Frontier 2. It was made using JAMESCRIPT.`, -5),
+                                paragraph(
+`Overall, I'm proud of this game. The resulting game is fun (if simplistic) and it also shows a bit how my code has improved. Both how it got better since then and how it improved between Rummikub and it.`,
+                                171),
+                                paragraph(
+`But yes, the golobal variables are confusing. Some are JAMESCRIPT methods and some are to tell it which assets and sprites to make (I think?).`,
+                                272),
+
+                                ...projectBar("Frontier", true),
+                                backButton(500, 450 + 225, "black")
                             ],
-                            animationMain: githubHandler
-                        }
+                            scroll: {},
+                            ...hoverText(true)
+                        },
+                        ...transitionSubmenu("Frontier", "GitHub"),
+                        ...transitionSubmenu("Frontier", "Pages")
                     },
                     stateToActivate: "menu"
                 }
